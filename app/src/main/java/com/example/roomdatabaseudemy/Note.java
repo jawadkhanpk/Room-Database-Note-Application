9ba1882584747014b1b2a027d04b049328a79ce4
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-08-23 12:54:50 +0500

    Initial commit

---
+++ app/src/main/java/com/example/roomdatabaseudemy/Note.java
@@ -0,0 +1,43 @@
+package com.example.roomdatabaseudemy;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
+public class Note {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+    public String title;
+    public String description;
+
+    public Note(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
